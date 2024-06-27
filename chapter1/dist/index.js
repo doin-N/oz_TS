20b1d3f00ef87f6aacf82898d83934e210b38846
doin-N <tlatms777@gmail.com>
2024-06-27 11:24:56 +0900

    TS 1DAY MS

---
+++ chapter1/dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+// console.log("hello TypeScript");
+Object.defineProperty(exports, "__esModule", { value: true });
+// const nums: number[] = [];
+// const a = 10 + nums[0];
+// const someString = "typeScript!";
+// someString.endsWith("!");
+// 스트링 메소드로 스트링의 마지막 글자가 내가 지정하는 글자와 일치하냐 이 글자로 끝나냐
+const util_1 = require("./util");
+(0, util_1.printName)("typescript");
---
+++ chapter1/src/index.ts
@@ -0,0 +1,8 @@
+//문자열 (string)
+
+const a: string = "";
+const b: string = "";
+const c: string = ``;
+
+let myName: string = "doin";
+let message: string = `Hello, ${myName}`;
